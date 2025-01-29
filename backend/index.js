30ddb47ebdbe354b8aad462f4de888cf36070435
Surender Pratap Singh <rk31151@gmail.com>
2025-01-29 15:48:06 +0530

    socket implemented

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,23 @@
 require("dotenv").config();
 const express = require("express");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
 const mongoose = require("mongoose");
-const app = express();
+
 const cors = require("cors");
 const authRouter = require("./routes/auth.routes");
+const Message = require("./models/Messages");
+
+const app = express();
+//http server
+const httpServer = createServer(app);
+//socket server
+const io = new Server(httpServer, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"],
+    },
+});
 
 app.use(cors());
 app.use(express.json());
@@ -38,7 +52,58 @@ app.use((err, req, res, next) => {
     });
 });
 
-app.listen(8800, () => {
+//socket logic
+io.on("connection", (socket) => {
+    console.log("a user connected", socket.id);
+
+    socket.on("send-message", async (data) => {
+        const { senderId, receiverId, text } = data;
+        try {
+            const message = new Message({
+                sender: senderId,
+                receiver: receiverId,
+                text,
+            });
+            await message.save();
+        } catch (error) {
+            console.log(error);
+        }
+
+        socket.broadcast.emit("receive-message", data);
+    });
+
+    socket.on("disconnect", () => {
+        console.log("a user disconnected", socket.id);
+    });
+});
+
+app.get("/api/messages", async (req, res) => {
+    const { senderId, receiverId } = req.query;
+    try {
+        const messages = await Message.find({
+            $or: [
+                { sender: senderId, receiver: receiverId },
+                { sender: receiverId, receiver: senderId },
+            ],
+        }).sort({ createdAt: 1 });
+        res.status(200).json(messages);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: error.message });
+    }
+});
+
+app.get("/api/users", async (req, res) => {
+    try {
+        const users = await User.find();
+        res.status(200).json(users);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: error.message });
+    }
+});
+
+httpServer.listen(8800, () => {
     connect();
     console.log("Connected to backend!");
 });
---
+++ backend/models/Messages.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const messageSchema = new mongoose.Schema(
+    {
+        sender: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+        },
+        receiver: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+        },
+        text: {
+            type: String,
+            required: true,
+        },
+    },
+    {
+        timestamps: true,
+    },
+);
+
+const Message = mongoose.model("Message", messageSchema);
+
+module.exports = Message;
